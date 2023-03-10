5bbe078053b33573fee64f70b7af4a78aec19a42
PlankXX71 <116320207+PlankXX71@users.noreply.github.com>
2023-03-10 11:44:45 +0700

    Create config.php

--- storm-web/config.php
+++ storm-web/config.php
@@ -2,8 +2,8 @@
 
 $CONFIG = array (
     "admin" => [
-        "fullname" => "hacker", 
-        "password" => "admin",
+        "fullname" => "hacker123", 
+        "password" => "adh",
     ], 
     
 );
